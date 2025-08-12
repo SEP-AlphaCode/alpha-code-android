0b897685746fb28faaf206e269e54c15879f56f0
DTNgoc-II0A <dangngoc4332@gmail.com>
2025-08-12 15:26:00 +0700

    Connect from outside

---
+++ app/src/main/java/com/ubtrobot/mini/sdkdemo/receiver/BootReceiver.java
@@ -0,0 +1,18 @@
+package com.ubtrobot.mini.sdkdemo.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.ubtrobot.mini.sdkdemo.service.RobotWebSocketService;
+
+// Create a BroadcastReceiver to start service on boot
+public class BootReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            context.startService(new Intent(context, RobotWebSocketService.class));
+        }
+    }
+}
