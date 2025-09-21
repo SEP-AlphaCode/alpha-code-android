c03f5fd3fc9a944cd487a36874008bc9bf29c734
DTNgoc-II0A <dangngoc4332@gmail.com>
2025-09-21 23:09:33 +0700

    Finally get face recognition to work
    Added activity to help registering the face

--- app/src/main/java/com/ubtrobot/mini/sdkdemo/speech/DemoSynthesizer.java
+++ app/src/main/java/com/ubtrobot/mini/sdkdemo/speech/DemoSynthesizer.java
@@ -3,6 +3,7 @@ package com.ubtrobot.mini.sdkdemo.speech;
 import android.util.Log;
 import com.ubtrobot.mini.sdkdemo.MainActivity;
 import com.ubtrobot.mini.sdkdemo.custom.tts.EnglishTTS;
+import com.ubtrobot.mini.sdkdemo.custom.tts.TTSCallback;
 import com.ubtrobot.speech.AbstractSynthesizer;
 import com.ubtrobot.speech.SpeakingVoice;
 import com.ubtrobot.speech.SynthesisOption;
@@ -15,8 +16,22 @@ public class DemoSynthesizer extends AbstractSynthesizer {
 
     @Override
     protected void startSynthesizing(SynthesisOption synthesisOption) {
-        EnglishTTS.getInstance().doTTS(synthesisOption.getInputText());
-        Log.d("Synthesizer", synthesisOption.toString());
+        EnglishTTS.getInstance().doTTS(synthesisOption.getInputText(), new TTSCallback() {
+            @Override
+            public void onStart() {
+
+            }
+
+            @Override
+            public void onDone() {
+                resolveSynthesizing();
+            }
+
+            @Override
+            public void onError() {
+
+            }
+        });
     }
 
     @Override
