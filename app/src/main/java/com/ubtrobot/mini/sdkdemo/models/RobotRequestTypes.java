b2a494a7c9b58516955e1dc285b17d1002affec0
DTNgoc-II0A <dangngoc4332@gmail.com>
2025-10-21 16:03:12 +0700

    Update RobotRequestTypes.java

--- app/src/main/java/com/ubtrobot/mini/sdkdemo/models/RobotRequestTypes.java
+++ app/src/main/java/com/ubtrobot/mini/sdkdemo/models/RobotRequestTypes.java
@@ -4,27 +4,27 @@ public class RobotRequestTypes {
     /**
      * Required field: asr
      */
-    public static final String PROCESS_SPEECH = "process_speech";
+    public static final String PROCESS_SPEECH = "process-speech";
     /**
      * Required fields:
      * 1. image
      * 2. params
      * - lang: target language (en, vi)
      */
-    public static final String DETECT_OBJECT = "detect_object";
+    public static final String DETECT_OBJECT = "detect-object";
     /**
      * Required fields: image
      */
-    public static final String PARSE_OSMO = "parse_osmo";
+    public static final String PARSE_OSMO = "parse-osmo";
     /**
      * Required fields: params
      * - serial: Serial of the robot
      */
-    public static final String NOTIFY_SHUTDOWN = "notify_shutdown";
+    public static final String NOTIFY_SHUTDOWN = "notify-shutdown";
     /**
      * Required fields: image
      */
-    public static final String PARSE_QR = "parse_qr";
+    public static final String PARSE_QR = "parse-qr";
     /**
      * A boot time ping to establish the connection
      */
